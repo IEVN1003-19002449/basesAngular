8c4cd45c91ee479e2631250159cbf0856f2a47c4
Griselda Ibarra <reyibme@gmail.com>
2024-09-20 18:24:04 -0600

    Commit 20 de septiembre multiplicar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';  // Importa ReactiveFormsModule
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    FormsModule,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    Ejemplo1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,           // Mantiene FormsModule
+    ReactiveFormsModule    // Añade ReactiveFormsModule aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -3,56 +3,53 @@ import { IProducto } from '../producto';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl:'./product-list.component.html', 
-  styleUrl: './product-list.component.css'
+  templateUrl: './product-list.component.html', 
+  styleUrls: ['./product-list.component.css']  // Corregí 'styleUrl' a 'styleUrls'
 })
 export class ProductListComponent {
-  title="Saludo de variable";
+  title = "Saludo de variable";
 
-  imageWidth:number=50;
-  ImageMargin:nusmber=2;
-  muestraIMG:boolean=true;
-  listFilter:string='';
-  
+  imageWidth: number = 50;
+  imageMargin: number = 2;  // Cambié 'ImageMargin' a 'imageMargin' (convención de camelCase)
+  muestraIMG: boolean = true;
+  listFilter: string = '';
 
-  showImage():void{
-    this.muestraIMG=!this.muestraIMG;
+  // Método para mostrar/ocultar imagen
+  showImage(): void {
+    this.muestraIMG = !this.muestraIMG;
   }
 
-  productos:IProducto[] = [
+  // Lista de productos
+  productos: IProducto[] = [
     {
-
-      "producto":1,
+      "producto": 1,
       "modelo": 'Sentra',
-      "descripcion":'4 puertas familiar',
-      "year":'febrero 3 2022',
-      "precio":200000,
-      "marca":'NISSAN',
-      "color":'morado',
-      "imagenUrl":"https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fmx.pinterest.com%2Fpin%2Fhoops-gets-a-facelift--782078291517274271%2F&psig=AOvVaw0ngIKnu9UmG-Qnmnb9EBIL&ust=1726708255839000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKjM_9Gny4gDFQAAAAAdAAAAABAZ"
+      "descripcion": '4 puertas familiar',
+      "year": 'febrero 3 2022',
+      "precio": 200000,
+      "marca": 'NISSAN',
+      "color": 'morado',
+      "imagenUrl": "https://example.com/sentra.png"
     },
     {
-
-      "producto":2,
+      "producto": 2,
       "modelo": 'A4',
-      "descripcion":'2 puertas familiar',
-      "year":'Marzo 3 2023',
-      "precio":500000,
-      "marca":'NISSAN',
-      "color":'blanco',
-      "imagenUrl":"https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.kavak.com%2Far%2Fusados%2Fnissan%2Fa4%2Fpickup&psig=AOvVaw1jMVZX2HQ1cRszTSiNQEez&ust=1726708302808000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJC3muiny4gDFQAAAAAdAAAAABAF"
+      "descripcion": '2 puertas familiar',
+      "year": 'Marzo 3 2023',
+      "precio": 500000,
+      "marca": 'NISSAN',
+      "color": 'blanco',
+      "imagenUrl": "https://example.com/a4.png"
     },
     {
-
-      "producto":3,
+      "producto": 3,
       "modelo": 'Rio',
-      "descripcion":'2 puertas familiar',
-      "year":'Agosto 3 2024',
-      "precio":600000,
-      "marca":'KIA',
-      "color":'Azul',
-      "imagenUrl":"https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.motorpasion.com.mx%2Findustria%2Fkia-rio-2021&psig=AOvVaw0BTXYKdYS58ECe-sZwRQ7Y&ust=1726708329987000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjvl_Wny4gDFQAAAAAdAAAAABAE"
+      "descripcion": '2 puertas familiar',
+      "year": 'Agosto 3 2024',
+      "precio": 600000,
+      "marca": 'KIA',
+      "color": 'Azul',
+      "imagenUrl": "https://example.com/rio.png"
     }
-
-  ]
+  ];
 }
